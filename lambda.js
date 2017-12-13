d89f27edb15ff6d128d3ab3320403f373e6f3012
Marie Byleen <6962330+mbyleen@users.noreply.github.com>
2017-12-13 14:35:46 -0600

    Move logic to filter for only actions done by IAM users from lambda function to CloudWatch event pattern

--- lambda.js
+++ lambda.js
@@ -4,16 +4,11 @@ var aws = require('aws-sdk');
 var iam = new aws.IAM();
 
 exports.handler = function(event, context) {
-    // Log the incoming Amazon CloudWatch Events event     
+    // Log the incoming Amazon CloudWatch Events event
     console.log('Received event:', JSON.stringify(event, null, 2));
-        
-    // If the caller is not an IAM user, do nothing
-    if (event.detail.userIdentity.type != 'IAMUser') {
-        context.done();
-    } else {
-        var userName = event.detail.userIdentity.userName;
 
-    // If the user is adding herself to a group
+    // If the user is adding themself to a group
+    var userName = event.detail.userIdentity.userName;
     if (event.detail.eventName === "AddUserToGroup" &&
         event.detail.requestParameters.userName === userName) {
 
@@ -34,4 +29,4 @@ exports.handler = function(event, context) {
             }
         });
     }
-}    
+}
