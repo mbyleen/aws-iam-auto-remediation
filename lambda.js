62331a4cfb8b565a47b10c52706c32416351ded4
Marie Byleen <6962330+mbyleen@users.noreply.github.com>
2017-12-15 16:06:23 -0600

    Revert move select iam user logic change

--- lambda.js
+++ lambda.js
@@ -4,29 +4,35 @@ var aws = require('aws-sdk');
 var iam = new aws.IAM();
 
 exports.handler = function(event, context) {
-    // Log the incoming Amazon CloudWatch Events event
+    // Log the incoming Amazon CloudWatch Events event     
     console.log('Received event:', JSON.stringify(event, null, 2));
 
-    // If the user is adding themself to a group
-    var userName = event.detail.userIdentity.userName;
-    if (event.detail.eventName === "AddUserToGroup" &&
-        event.detail.requestParameters.userName === userName) {
+     // If the caller is not an IAM user, do nothing
+     if (event.detail.userIdentity.type != 'IAMUser') {
+         context.done();
+     } else {
+        var userName = event.detail.userIdentity.userName;
 
-        // Remove the user from that group
-        var groupName = event.detail.requestParameters.groupName;
-        console.log('User adding self to group detected. Removing user',
-            userName, 'from group', groupName);
+        // If the user is adding herself to a group
+        if (event.detail.eventName === "AddUserToGroup" &&
+                event.detail.requestParameters.userName === userName) {
 
-        var params = {
-            GroupName: groupName, 
-            UserName: userName
-        };
-        iam.removeUserFromGroup(params, function(err, data) {
-            if (err) {
-                console.log(err, err.stack);
-            } else {
-                console.log(data);
-            }
-        });
-    }
-}
+            // Remove the user from that group
+            var groupName = event.detail.requestParameters.groupName;
+            console.log('User adding self to group detected. Removing user',
+                            userName, 'from group', groupName);
+
+            var params = {
+                GroupName: groupName, 
+                UserName: userName
+            };
+            iam.removeUserFromGroup(params, function(err, data) {
+                if (err) {
+                    console.log(err, err.stack);
+                } else {
+                    console.log(data);
+                }
+            });
+        }
+     }
+  }
